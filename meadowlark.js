48b6d3d19fa7c01419997db9f59786a289bd22a6
xgtz <15809207816@163.com>
2018-07-16 23:24:15 +0800

    '提交模板和静态文件'

--- meadowlark.js
+++ meadowlark.js
@@ -1,20 +1,30 @@
 var express = require('express');
 var app = express();
-
+// 设置handlebars 视图引擎
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+    console.log('main home');
+    res.render('home');
+});
+app.get('/about',function(req,res){
+    res.render('about');
+});
 // 定制404 页面
 app.use(function(req,res){
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 // 定制500错误
 app.use(function(err,req,res,next){
     console.log(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'),function(){
