6f584ec67602bcd3860581b1f89f9e797ef20771
xgtz <15809207816@163.com>
2018-07-25 22:53:33 +0800

    ch07

--- meadowlark.js
+++ meadowlark.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 var fortune = require('./lib/fortune.js');
 // 设置handlebars 视图引擎
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express3-handlebars')
+    .create({
+        defaultLayout:'main',
+        helpers:{
+            section:function(name,options){
+                if(!this._sections) this._sections ={};
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
+    });
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 app.set('port',process.env.PORT || 3000);
@@ -11,6 +21,41 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function(req,res,next){
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+// mocked weather data
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+app.use(function(req,res,next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
     next();
 });
 
@@ -30,6 +75,23 @@ app.get('/tours/hood-river',function(req,res){
 
 app.get('/tours/request-group-rate',function(req,res){
     res.render('request-group-rate');
+});
+
+app.get('/jquery-test',function(req,res){
+    res.render('jquerytest');
+});
+
+app.get('/nursery-rhyme',function(req,res){
+    res.render('nursery-rhyme');
+});
+
+app.get('/data/nursery-rhyme',function(req,res){
+    res.json({
+        animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+    });
 });
 
 // 定制404 页面
